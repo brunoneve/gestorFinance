776392bb9c677d92a543d289fd10942209b9f348
Bruno Neves <brunu.neves@gmail.com>
2017-09-28 21:01:39 -0300

    init

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'gestor_finance',
+        'username'  => 'root',
+        'password'  => 'qwer123',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
---
+++ db/migrations/20170928233114_create_category_costs.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateCategoryCosts extends AbstractMigration
+{
+
+    public function up()
+    {
+        $this->table('category_costs')
+            ->addColumn('name', 'string')
+            ->addColumn('created_at','datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->save();
+
+    }
+
+    public function down()
+    {
+        $this->dropDatabase('category_costs');
+    }
+}
