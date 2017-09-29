277ce446b2710b018826acf583be95e1a1592c4e
Bruno Neves <brunu.neves@gmail.com>
2017-09-29 00:15:35 -0300

    added psr7 zend-diactoros

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,9 +1,12 @@
 <?php
-
+declare(strict_types = 1);
 namespace GestorFin\Plugins;
 
 
 use Aura\Router\RouterContainer;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Zend\Diactoros\ServerRequestFactory;
 use GestorFin\ServiceContainerInterface;
 
 class RoutePlugin implements PluginInterface
@@ -21,9 +24,27 @@ class RoutePlugin implements PluginInterface
         $matcher = $routerContainer->getMatcher();
         /** @var $generator  - generate links based on registered routes */
         $generator = $routerContainer->getGenerator();
+        $request = $this->getRequest();
 
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
         $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+            return $matcher->match($request);
+        });
+    }
+
+    /**
+     * @return RequestInterface
+     */
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
